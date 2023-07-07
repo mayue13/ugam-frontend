71315af11939a48debf36fd846c296b371b5f85e
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2023-07-08 00:34:15 +0530

    events are added

--- src/components/NavigationDots/NavigationDots.jsx
+++ src/components/NavigationDots/NavigationDots.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function NavigationDots({ active }) {
   return (
     <div className="app__navigation">
-      {["home", "about", "events", "gallary", "contact"].map((item, index) => (
+      {["home", "about", "event", "gallary", "contact"].map((item, index) => (
         <a
           href={`#${item}`}
           key={item + index}
--- src/container/Event/Event.jsx
+++ src/container/Event/Event.jsx
@@ -9,18 +9,18 @@ import { urlFor, client } from "../../client";
 function Event() {
   const [activeFilter, setActiveFilter] = useState("All");
   const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
-  const [works, setWorks] = useState([]);
-  const [filterWork, setFilterWork] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [filterEvent, setFilterEvent] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type == "works"]';
+    const query = '*[_type == "events"]';
     client.fetch(query).then((data) => {
-      setWorks(data);
-      setFilterWork(data);
+      setEvents(data);
+      setFilterEvent(data);
     });
   }, []);
 
-  const handleWorkFilter = (item) => {
+  const handleEventFilter = (item) => {
     setActiveFilter(item);
     setAnimateCard([{ y: 100, opacity: 0 }]);
 
@@ -28,9 +28,9 @@ function Event() {
       setAnimateCard([{ y: 0, opacity: 1 }]);
 
       if (item === "All") {
-        setFilterWork(works);
+        setFilterEvent(events);
       } else {
-        setFilterWork(works.filter((work) => work.tags.includes(item)));
+        setFilterEvent(events.filter((event) => event.tags.includes(item)));
       }
     }, 500);
   };
@@ -38,35 +38,32 @@ function Event() {
   return (
     <>
       <h2 className="head-text">
-        My Creative <span>Work</span> Section
+        Events <span>Ugam</span> Played
       </h2>
 
-      <div className="app__work-filter">
-        {["Web App", "HTML/CSS", "React JS", ".Net Core", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
+      <div className="app__event-filter">
+        {["Ganpati", "Shiv Jayanti", "Corporate", "All"].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleEventFilter(item)}
+            className={`app__event-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
       </div>
 
       <motion.div
         animate={animateCard}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__work-portfolio"
+        className="app__event-portfolio"
       >
-        {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
-
+        {filterEvent.map((event, index) => (
+          <div className="app__event-item app__flex" key={index}>
+            <div className="app__event-img app__flex">
+              <img src={urlFor(event.imgUrl)} alt={event.name} />
               <motion.div
                 whileHover={{ opacity: [0, 1] }}
                 transition={{
@@ -74,9 +71,9 @@ function Event() {
                   ease: "easeInOut",
                   staggerChildren: 0.5,
                 }}
-                className="app__work-hover app__flex"
+                className="app__event-hover app__flex"
               >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
+                <a href={event.eventink} target="_blank" rel="noreferrer">
                   <motion.div
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
@@ -86,27 +83,16 @@ function Event() {
                     <AiFillEye />
                   </motion.div>
                 </a>
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ scale: [0, 1] }}
-                    whileHover={{ scale: [1, 0.9] }}
-                    transition={{ duration: 0.5 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
               </motion.div>
             </div>
-
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
+            <div className="app__event-content app__flex">
+              <h4 className="bold-text">{event.title}</h4>
               <p className="p-text" style={{ marginTop: 10 }}>
-                {work.description}
+                {event.description}
               </p>
 
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
+              <div className="app__event-tag app__flex">
+                <p className="p-text">{event.tags[0]}</p>
               </div>
             </div>
           </div>
@@ -117,7 +103,7 @@ function Event() {
 }
 
 export default AppWrap(
-  MotionWrap(Event, "app__works"),
+  MotionWrap(Event, "app__events"),
   "event",
   "app__primarybg"
 );
