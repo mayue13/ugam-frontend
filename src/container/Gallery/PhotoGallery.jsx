40da03647fee42b19701e2c0dd51dd3fdf45ba10
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2023-07-10 22:13:19 +0530

    gallary

--- src/container/Gallery/PhotoGallery.jsx
+++ src/container/Gallery/PhotoGallery.jsx
@@ -1,11 +1,25 @@
+import React, { useState, useEffect } from "react";
 import { GalleryImages } from "../../constants/index";
 import Gallery from "react-photo-gallery";
 
 function PhotoGallery() {
+  const itemsPerPage = 7;
+  const totalPages = Math.ceil(GalleryImages.length / itemsPerPage);
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentPhotos = GalleryImages.slice(startIndex, endIndex);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <>
-      <Gallery photos={GalleryImages} />
-    </>
+    <div className="img-container">
+      <Gallery photos={currentPhotos} />
+    </div>
   );
 }
 
