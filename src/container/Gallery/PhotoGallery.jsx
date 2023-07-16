a2f30461886cf9506adf403c6cfe70796db5578f
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2023-07-16 15:27:10 +0530

    code updated

--- src/container/Gallery/PhotoGallery.jsx
+++ src/container/Gallery/PhotoGallery.jsx
@@ -3,26 +3,6 @@ import { GalleryImages } from "../../constants/index";
 import { Carousel } from "react-carousel-minimal";
 
 function PhotoGallery() {
-  const itemsPerPage = 7;
-  const [currentPage, setCurrentPage] = useState(1);
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-  const totalPages = Math.ceil(GalleryImages.length / itemsPerPage);
-  const currentPhotos = GalleryImages.slice(startIndex, endIndex);
-
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const handlePreviousPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
-  };
-
-  const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
-  };
-
   const captionStyle = {
     fontSize: "2em",
     fontWeight: "bold",
@@ -33,26 +13,6 @@ function PhotoGallery() {
   };
 
   return (
-    // <div className="img-container">
-    //   {/* <Gallery photos={currentPhotos} /> */}
-    //   <div className="pagination">
-    //     <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-    //       &laquo;
-    //     </button>
-    //     {Array.from({ length: totalPages }, (_, index) => (
-    //       <button
-    //         key={index + 1}
-    //         onClick={() => handlePageChange(index + 1)}
-    //         className={currentPage === index + 1 ? "active" : ""}
-    //       >
-    //         {index + 1}
-    //       </button>
-    //     ))}
-    //     <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-    //       &raquo;
-    //     </button>
-    //   </div>
-    // </div>
     <div>
       <Carousel
         data={GalleryImages}
