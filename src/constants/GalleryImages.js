7727dc442b377f654d00a294c14996cf92afccc7
mayur krushnarao dahake <35840117+mayue13@users.noreply.github.com>
2023-07-22 12:31:42 +0530

    Images are added

--- src/constants/GalleryImages.js
+++ src/constants/GalleryImages.js
@@ -2,6 +2,12 @@ import headImage3 from "../assets/images/header3.JPG";
 import headImage1 from "../assets/images/header1.jpg";
 import headImage2 from "../assets/images/header2.JPG";
 import headImage4 from "../assets/images/header4.JPG";
+import headImage5 from "../assets/images/header5.JPG";
+import headImage6 from "../assets/images/header6.JPG";
+import headImage7 from "../assets/images/header7.JPG";
+import headImage8 from "../assets/images/header8.JPG";
+import headImage9 from "../assets/images/header9.JPG";
+import headImage10 from "../assets/images/header10.JPG";
 
 const GalleryImages = [
   {
@@ -15,6 +21,18 @@ const GalleryImages = [
   },
   {
     image: headImage4,
+  },
+  {
+    image: headImage6,
+  },
+  {
+    image: headImage7,
+  },
+  {
+    image: headImage8,
+  },
+  {
+    image: headImage9,
   },
 ];
 
